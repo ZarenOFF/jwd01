25acc854d5954350e6b28ec0f65c1dd129192fa5
ZarenOFF <f.o.b.o@yandex.ru>
2021-10-07 15:39:19 +0300

    Added task7, refactored task10

--- src/by/training/fundamentals/task1/step1/task10.java
+++ src/by/training/fundamentals/task1/step1/task10.java
@@ -6,38 +6,37 @@ import java.util.Scanner;
 
 public class task10 {
     public static void main(String[] args) {
-        int n;
+        int size;
 
-        System.out.println("Enter n");
-        n = enterNum();
+        System.out.println("Enter size");
+        size = enterNum();
 
-        int arr[][] = new int[n][n];
+        int arr[][] = new int[size][size];
 
         for (int i = 0; i < arr.length; i++) {
-            if (i % 2 == 0){
+            if (i % 2 == 0) {
                 for (int j = 0; j < arr[i].length; j++) {
                     arr[i][j] = j + 1;
                 }
             } else {
-                int t = 1;
-                for (int j = arr[i].length-1; j >= 0; j--) {
-                    arr[i][j] = t;
-                    t++;
+                int beginNum = 1;
+                for (int j = arr[i].length - 1; j >= 0; j--) {
+                    arr[i][j] = beginNum;
+                    beginNum++;
                 }
             }
         }
 
         for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr[i].length; j++) {
-                    System.out.print("["+arr[i][j]+"]");
-                }
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print("[" + arr[i][j] + "]");
+            }
             System.out.println();
         }
     }
 
     private static int enterNum() {
         Scanner scan = new Scanner(System.in);
-
         while (!scan.hasNextInt()) {
             scan.nextLine();
             System.out.println("Try again");
---
+++ src/by/training/fundamentals/task1/step1/task7.java
@@ -0,0 +1,34 @@
+package by.training.fundamentals.task1.step1;
+
+import java.util.Scanner;
+
+public class task7 {
+    public static void main(String[] args) {
+        double result, x, step, beginLine, endLine, currentPosition;
+
+        System.out.println("Enter a");
+        beginLine = enterNum();
+        currentPosition = beginLine;
+        System.out.println("Enter b");
+        endLine = enterNum();
+        System.out.println("Enter step");
+        step = enterNum();
+
+        System.out.println(Math.sin(1.0));
+
+        while (currentPosition <= endLine) {
+            result = Math.pow(Math.sin(currentPosition), 2) - Math.cos(2 * currentPosition);
+            System.out.printf("|%.1f|%.4f|\n", currentPosition, result);
+            currentPosition += step;
+        }
+    }
+
+    private static double enterNum() {
+        Scanner scan = new Scanner(System.in);
+        while (!scan.hasNextInt()) {
+            scan.nextDouble();
+            System.out.println("Try again");
+        }
+        return scan.nextDouble();
+    }
+}
